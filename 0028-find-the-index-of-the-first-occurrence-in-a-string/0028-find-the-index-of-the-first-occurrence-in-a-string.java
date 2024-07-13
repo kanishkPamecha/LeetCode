5121030c1522c906b9214b4878bee5466309dcb9
Kanishk Pamecha <124118035+kanishkPamecha@users.noreply.github.com>
2024-07-13 19:33:14 +0530

    Time: 1 ms (31.03%), Space: 41.4 MB (34.28%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        for(int i =0 ;i<haystack.length() ; i++)
+        {
+            if(haystack.charAt(i)==needle.charAt(0))
+            {
+                for(int j = 0 ;j< needle.length() ;j++)
+                { 
+                    if(haystack.length()-1<i+j)return -1;
+                    if(haystack.charAt(i+j)!=needle.charAt(j)) break;
+
+                  if(j == needle.length()-1)  return i;
+                }
+            }
+        }
+        return -1;
+
+    }
+}
